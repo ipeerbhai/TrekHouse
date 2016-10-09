7cef9806bb9d8072895fad6a67d576daf5c3903f
Toby Liu <ybot1122@gmail.com>
2016-10-08 21:09:31 -0700

    url and contract

--- Webtasks/door.js
+++ Webtasks/door.js
@@ -1,3 +1,9 @@
+/*
+https://wt-23birdsonfire-gmail-com-0.run.webtask.io/door?webtask_no_cache=1
+  POST: (no body) will flip the state of the door
+  GET: returns JSON object: { doorOpen: boolean }
+*/
+
 module.exports = function(ctx, req, res) {
 
   if (req.method === "POST") {
